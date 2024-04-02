088526b79dd4ed6a3d8ff8be651645d17e7b0669
Om chaudhari <omchaudhari0003@gmail.com>
2024-04-02 21:35:52 +0530

    Day7

---
+++ Day7.js
@@ -0,0 +1,11 @@
+/**
+ * find maximum number array
+ */
+
+const findMax = (arr) => {
+  //return Math.max(1, 2, 3, 4);
+  console.log(...arr);
+  return Math.max(...arr);
+};
+
+console.log(findMax([1, 23, 4, 56, 3]));
